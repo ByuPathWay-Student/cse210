8168f16b556c19bcdb27a72fd22796964eb911d0
ByuPathWay-Student <ascott@byupathway.edu>
2026-01-08 16:25:57 -0700

    I completed the Week01 Exercise2 Activity.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+
+        int numberUserInput = int.Parse(userInput);
+        string letter = "";
+
+
+        if (numberUserInput >= 90)
+        {
+            letter = "A";
+        }
+        else if (numberUserInput >= 80)
+        {
+            letter = "B";
+        }
+        else if (numberUserInput >= 70)
+        {
+            letter = "C";
+        }
+        else if (numberUserInput >= 60)
+        {
+            letter = "D";
+        }
+        else if (numberUserInput < 60)
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your Grade is: {letter}");
+
+        if (numberUserInput >= 70)
+        {
+            Console.WriteLine("Congratulations, You Have passed the course!");
+        }
+        else if (numberUserInput < 70)
+        {
+            Console.WriteLine("You have not passed the course. Better luck next time!");
+        }
+
+
     }
 }
