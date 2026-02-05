f145c3ec962cf389ce9054783a17eca522f4756d
ByuPathWay-Student <ascott@byupathway.edu>
2026-02-04 18:57:26 -0700

    I completed the Week05 Learning activity.

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment = new Assignment("Timmy Wood", "Division");
+        MathAssignment mathAssignment = new MathAssignment("Duncan Warner", "Multiplication", "9", "2-5");
+        WritingAssignment writingAssignment = new WritingAssignment("Lesli Wendell", "English", "My short story");
+
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine();
+        
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+        Console.WriteLine();
+
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
