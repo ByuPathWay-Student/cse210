9fc2eb4880886b07090719c364e72f8bfbaaf3dc
ByuPathWay-Student <ascott@byupathway.edu>
2026-02-10 18:54:28 -0700

    I completed the Week06 Learning activity.

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+        
+        Square square = new Square("red", 12);
+        shapes.Add(square);
+        Rectangle rectangle = new Rectangle("purple", 5, 9);
+        shapes.Add(rectangle);
+        Circle circle = new Circle("orange", 3);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            Console.WriteLine($"Color: {color} Area: {area}");
+        }
     }
 }
